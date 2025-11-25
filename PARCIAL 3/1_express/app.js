2f1abb581cc3325e393b3bed3336357a9f0d58c3
Hiram GG <reaxorgg@gmail.com>
2025-11-25 07:13:31 -0600

    parcpsas

--- PARCIAL 3/1_express/app.js
+++ PARCIAL 3/1_express/app.js
@@ -1,11 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const cors = require ('cors');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors())
+app.use(express.json())
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/clientes', cors(),(req, res)=>{
+  res.send('hola')
+});
+
+app.get('/alumnos', (req, res) => {
+  console.log(req.query);
+  res.send('Hello Worlld!');
+});
+
+app.get('/docentes/:control', (req, res) => {
+  console.log(req.params);
+  res.send('Hello World!');
+});
+
+app.post('/directivos', (req, res) => {
+  console.log(req.body);
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port ${3000}`);
+});
