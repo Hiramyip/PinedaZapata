ebd4b61244d7ac8c95692b0cd93fb29af73e1e36
Hiram GG <reaxorgg@gmail.com>
2025-11-27 07:55:00 -0600

    haber si ajala

--- PARCIAL 3/1_express/app.js
+++ PARCIAL 3/1_express/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require ('cors');
-
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3000
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'aerocontrol',
+});
 
 app.use(cors())
 app.use(express.json())
@@ -11,9 +17,18 @@ app.get('/clientes', cors(),(req, res)=>{
   res.send('hola')
 });
 
+
 app.get('/alumnos', (req, res) => {
-  console.log(req.query);
-  res.send('Hello Worlld!');
+  
+connection.query(
+  'SELECT * FROM `vuelo` WHERE `id_vuelo` = ?',
+  ['3'],
+  function (err, results) {
+    console.log(results);
+  }
+);
+  //console.log(req.query);
+  //res.send('Hello Worlld!');
 });
 
 app.get('/docentes/:control', (req, res) => {
